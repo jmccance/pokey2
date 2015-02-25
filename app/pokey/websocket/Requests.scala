8bf6818f85135b23116852501c38932d5d6ed723
Joel McCance <joelm@slalom.com>
2015-02-24 20:33:12 -0600

    Implemented remainder of request types.

--- app/pokey/websocket/Requests.scala
+++ app/pokey/websocket/Requests.scala
@@ -15,7 +15,12 @@ object Request {
 
   implicit val formatter = Format[Request](
     SetName.reader
+      orElse CreateRoom.reader
       orElse JoinRoom.reader
+      orElse Estimate.reader
+      orElse Reveal.reader
+      orElse Hide.reader
+      orElse Clear.reader
       orElse InvalidRequest.reader,
     Writes[Request](_ => ???)
   )
@@ -24,24 +29,67 @@ object Request {
 object Requests {
   object RequestType {
     val setName = "setName"
+    val createRoom = "createRoom"
     val joinRoom = "joinRoom"
+    val estimate = "estimate"
+    val reveal = "reveal"
+    val hide = "hide"
+    val clear = "clear"
   }
 
   case class SetName(name: String) extends Request
 
   object SetName {
     val reader: Reads[Request] =
-      ((JsPath \ "request").read[String].filter(_ == RequestType.setName) andKeep
-        (JsPath \ "name").read[String]).map(SetName(_))
+      ((JsPath \ "request").read[String].filter(_ == RequestType.setName)
+        andKeep (JsPath \ "name").read[String].map(SetName(_)))
   }
 
-  case class JoinRoom(id: String) extends Request
+  case object CreateRoom extends Request {
+    val reader: Reads[Request] =
+      ((JsPath \ "request").read[String].filter(_ == RequestType.createRoom)
+        andKeep Reads.pure(CreateRoom))
+  }
+
+  case class JoinRoom(roomId: String) extends Request
 
   object JoinRoom {
     val reader: Reads[Request] =
       ((JsPath \ "request").read[String].filter(_ == RequestType.joinRoom)
-        andKeep (JsPath \ "id").read[String]).map(JoinRoom(_))
+        andKeep (JsPath \ "roomId").read[String]).map(JoinRoom(_))
   }
 
-  case object CreateRoom
+  case class Estimate(roomId: String, value: String, comment: String) extends Request
+
+  object Estimate {
+    val reader: Reads[Request] =
+      ((JsPath \ "request").read[String].filter(_ == RequestType.estimate)
+        andKeep (JsPath \ "roomId").read[String]
+        and (JsPath \ "value").read[String]
+        and (JsPath \ "comment").read[String])(Estimate.apply _)
+  }
+  
+  case class Reveal(roomId: String) extends Request
+  
+  object Reveal {
+    val reader: Reads[Request] =
+      ((JsPath \ "request").read[String].filter(_ == RequestType.reveal)
+        andKeep (JsPath \ "roomId").read[String]).map(Reveal(_))
+  }
+  
+  case class Hide(roomId: String) extends Request
+  
+  object Hide {
+    val reader: Reads[Request] =
+      ((JsPath \ "request").read[String].filter(_ == RequestType.hide)
+        andKeep (JsPath \ "roomId").read[String]).map(Hide(_))
+  }
+  
+  case class Clear(roomId: String) extends Request
+  
+  object Clear {
+    val reader: Reads[Request] =
+      ((JsPath \ "request").read[String].filter(_ == RequestType.clear)
+        andKeep (JsPath \ "roomId").read[String]).map(Clear(_))
+  }
 }
