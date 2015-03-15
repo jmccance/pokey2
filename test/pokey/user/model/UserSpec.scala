9b37711e5b6ee3288255ece4077e238fa5a7c9c0
Joel McCance <joelm@slalom.com>
2015-03-15 11:34:42 -0500

    [Issue #5] Implemented UserSpec.

---
+++ test/pokey/user/model/UserSpec.scala
@@ -0,0 +1,19 @@
+package pokey.user.model
+
+import play.api.libs.json.{JsString, Json}
+import pokey.test.UnitSpec
+
+class UserSpec extends UnitSpec {
+
+  "A User" when {
+    "serialized to JSON" should {
+      "contain the expected fields" in {
+        val user = User("1234", "Antoine")
+        val json = Json.toJson(user)
+
+        (json \ "id") shouldBe JsString(user.id)
+        (json \ "name") shouldBe JsString(user.name)
+      }
+    }
+  }
+}
