67db75c961abec5b56d41bac143ed9d9f8542f51
Joel McCance <joelm@slalom.com>
2015-03-15 16:44:04 -0500

    [Issue #5] Added spec for AssetController.

--- test/pokey/test/specs.scala
+++ test/pokey/test/specs.scala
@@ -4,20 +4,24 @@ import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest._
 import concurrent.ScalaFutures
+import mock.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import play.api.mvc.Results
 
 trait BaseSpec
   extends WordSpecLike
-  with Matchers
   with OptionValues
   with Inspectors
   with ScalaFutures
+  with MockitoSugar
 
-abstract class UnitSpec extends WordSpec with BaseSpec
+abstract class UnitSpec extends WordSpec with BaseSpec with Matchers
 
 abstract class AkkaUnitSpec(_system: ActorSystem)
   extends TestKit(_system)
   with ImplicitSender
   with BaseSpec
+  with Matchers
   with BeforeAndAfterAll {
 
   def this() = this(ActorSystem())
@@ -27,3 +31,8 @@ abstract class AkkaUnitSpec(_system: ActorSystem)
     TestKit.shutdownActorSystem(system)
   }
 }
+
+abstract class PlayUnitSpec
+  extends PlaySpec
+  with BaseSpec
+  with Results
