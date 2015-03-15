5bf1e484971e8ed349e15e406afcda696c58bd68
Joel McCance <joelm@slalom.com>
2015-03-15 12:34:22 -0500

    [Issue #5] Slight refactor of modules.

--- app/pokey/application/Global.scala
+++ app/pokey/application/Global.scala
@@ -1,23 +1,16 @@
 package pokey.application
 
-import _root_.akka.actor.ActorSystem
 import _root_.play.api.libs.concurrent.Akka
+import module.{AkkaModule, ServiceModule, WebModule}
 import play.api.GlobalSettings
 import play.api.Play.current
-import play.api.libs.concurrent.Execution.Implicits
-import pokey.application.module.{ServiceModule, WebModule}
 import scaldi._
 import scaldi.play.ScaldiSupport
 
-import scala.concurrent.ExecutionContext
-
 object Global extends GlobalSettings with ScaldiSupport {
-  override def applicationModule: Injector = {
-    val contextModule = new Module {
-      bind [ActorSystem] to Akka.system
-      bind [ExecutionContext] to Implicits.defaultContext
-    }
-
-    contextModule :: new ServiceModule :: new WebModule
-  }
+  override def applicationModule: Injector = (
+    new AkkaModule(Akka.system)
+      :: new ServiceModule
+      :: new WebModule
+  )
 }
---
+++ app/pokey/application/module/AkkaModule.scala
@@ -0,0 +1,8 @@
+package pokey.application.module
+
+import akka.actor.ActorSystem
+import scaldi.Module
+
+class AkkaModule(system: ActorSystem) extends Module {
+  bind [ActorSystem] to system
+}
