971ea3f22eccea41a97db00657df9295d65a1bc6
Joel McCance <joelm@slalom.com>
2015-03-22 20:55:05 -0500

    Basic implementation of PokeyApi front-end client.

--- app/assets/js/util/PokeyApi.js
+++ app/assets/js/util/PokeyApi.js
@@ -11,7 +11,88 @@ export default class EventEmitter {
 
         case RoomCreated:
           break;
+
+        case RoomUpdatedEvent:
+          break;
+
+        case RoomUpdatedEvent:
+          break;
+
+        case UserJoinedEvent:
+          break;
+
+        case UserLeftEvent:
+          break;
+
+        case EstimateUpdatedEvent:
+          break;
+
+        case RoomRevealedEvent:
+          break;
+
+        case RoomClearedEvent:
+          break;
+
+        case RoomClosedEvent:
+          break;
+
+        case ErrorEvent:
+          break;
       }
     };
+  }
+
+  setName(name) {
+    const msg = {
+      command: 'setName',
+      name: name
+    };
+
+    this.conn.send(JSON.stringify(msg));
+  }
+
+  createRoom() {
+    const msg = {
+      command: 'createRoom'
+    };
+
+    this.conn.send(JSON.stringify(msg));
+  }
+
+  joinRoom(roomId) {
+    const msg = {
+      command: 'joinRoom',
+      roomId: roomId
+    };
+
+    this.conn.send(JSON.stringify(msg));
+  }
+
+  submitEstimate(roomId, estimate) {
+    const msg = {
+      command: 'submitEstimate',
+      roomId: roomId,
+      estimate: estimate
+    };
+
+    this.conn.send(JSON.stringify(msg));
+  }
+
+  revealRoom(roomId) {
+    const msg = {
+      command: 'revealRoom',
+      roomId: roomId
+    };
+
+    this.conn.send(JSON.stringify(msg));
+  }
+
+  clearRoom(roomId) {
+    const msg = {
+      command: 'clearRoom',
+      roomId: roomId
+    };
+
+    this.conn.send(JSON.stringify(msg));
   }
 }
