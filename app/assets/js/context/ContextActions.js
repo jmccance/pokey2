174cd5ad12c70642bcc2b86ba49a64a140ae10f0
Joel McCance <joelm@slalom.com>
2015-03-24 10:26:25 -0500

    Added some JSDoc and notes.

--- app/assets/js/context/ContextActions.js
+++ app/assets/js/context/ContextActions.js
@@ -1,1 +1,12 @@
 // Placeholder for Actions related to the Context. (E.g., route changes.)
+
+// NOTE: Based on the way external APIs are handled, this is probably a more appropriate point to integrate with the
+// router instead of directly coupling it to the ContextStore. Instead have Actions here for changing and getting the
+// context, with the certain context changes triggering or being triggered by route changes.
+
+import AppDispatcher from '../dispatcher/AppDispatcher';
+
+export default new class {
+  setCurrentUser(user) {
+  }
+}
--- app/assets/js/context/ContextStore.js
+++ app/assets/js/context/ContextStore.js
@@ -1,40 +1,61 @@
 import director from 'director';
 import EventEmitter from 'events';
 
+/**
+ * Store for the current application context, including the current view to display (lobby or room) and the current
+ * user.
+ *
+ * @module context/ContextStore
+ */
+
+/**
+ * Context type identifiers. Indicates which view of the application is currently active.
+ */
 export const ContextType = {
   lobby: 'lobby',
   room: 'room'
 };
+
+var _context;
 
 export default new class extends EventEmitter {
   constructor() {
     const routes = {
       '/': () => {
         console.log('router: /');
-        this._context = this._getContextFromRoute();
+        _context = this._getContextFromRoute();
         this.emitChange();
       },
       '/room/:roomId': (roomId) => {
         console.log(`router: /room/${roomId}`);
-        this._context = this._getContextFromRoute();
-        this.emitChange();
+        _context = this._getContextFromRoute();
+        this._emitChange();
       }
     };
 
     this._router = director.Router(routes);
     this._router.init('');
 
-    this._context = this._getContextFromRoute();
+    _context = this._getContextFromRoute();
   }
 
-  emitChange() {
+  /**
+   * @returns {String} the current context identifier
+   */
+  get() {
+    return _context;
+  }
+
+  /**
+   * Notify listeners that the store has changed.
+   */
+  _emitChange() {
     this.emit('route_updated');
   }
 
-  get() {
-    return this._context;
-  }
-
+  /**
+   * @returns {String} the current context string as determined by the current route
+   */
   _getContextFromRoute() {
     const route = this._router.getRoute(0);
 
