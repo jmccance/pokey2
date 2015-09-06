42f7913f8896f12c5b5fd3d4e5a79efce4a89305
Joel McCance <jmccance@gmail.com>
2015-09-06 10:47:58 -0500

    Converted EstimateForm to react-bootstrap

--- client/js/components/room/EstimateForm.js
+++ client/js/components/room/EstimateForm.js
@@ -1,48 +1,49 @@
 import React from 'react';
+import {Col, Grid, Row} from 'react-bootstrap';
 
 export default class extends React.Component {
   render() {
     return (
       <form name='estimate'
             data-role='form'>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-xs-1'>
+        <Grid fluid>
+          <Row>
+            <Col xs={1}>
               <label htmlFor='estimate-hours'>
                 Estimate
               </label>
-            </div>
+            </Col>
 
-            <div className='col-xs-2'>
+            <Col xs={2}>
               <input id='estimate-hours'
                      className='form-control'
                      type='number'
                      min='1'
                      max='12'
                      step='1' />
-            </div>
+            </Col>
 
-            <div className='col-xs-1'>
+            <Col xs={1}>
               <label htmlFor='estimate-comment'>
                   Comment
               </label>
-            </div>
+            </Col>
 
-            <div className='col-xs-7'>
+            <Col xs={7}>
               <input id='estimate-comment'
                      className='form-control'
                      pattern='.*\w.*'
                      type='text'
                      maxLength='255'/>
-            </div>
+            </Col>
 
-            <div className='col-xs-1'>
+            <Col xs={1}>
               <button className='btn btn-primary' type='submit'>
                   Submit
               </button>
-            </div>
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Grid>
       </form>
     );
   }
